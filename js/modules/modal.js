af87a4f5a6140c7d77d46de26380366b599206cf
Pegasis12 <plsantana246@gmail.com>
2026-01-06 10:07:22 -0300

    modal refatorado com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,24 +1,44 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector("[data-modal='abrir']");
-  const botaoFechar = document.querySelector("[data-modal='fechar']");
-  const containerModal = document.querySelector("[data-modal='container']");
-  // sempre cirar uma verificacao pra identificar se nossos seletores existem
-  function toggleModal(event) {
-    event.preventDefault();
-    containerModal.classList.toggle('ativo');
+export default class Modal {
+  constructor(botaoAbrir, botaoFechar, containerModal) {
+    this.botaoAbrir = document.querySelector(botaoAbrir);
+    this.botaoFechar = document.querySelector(botaoFechar);
+    this.containerModal = document.querySelector(containerModal);
+
+    //bind para referenciar o this correto das funcoes
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
   }
 
-  function cliqueForaModal(event) {
+  //abre ou fecha  modal
+  toggleModal() {
+    this.containerModal.classList.toggle('ativo');
+  }
+
+  //add o evento de togle ao modal
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  //fecha modal ao clicar do lado de ora
+  cliqueForaModal(event) {
     //container modal é a section que carrega modal, ao clicar nela fecha tbm, o problema disso é que q section carrega tudo, ent ao clicar no que a section carrega tbm fecha o modal, porem o event faz referecnai a section que o this principal, ent é so criarmos um if se o clique foi no this, se foi nele fecha se n n fecha
-    if (event.target === this) {
-      toggleModal(event); //ao clicar do lado de fora a funcao cliqueFora executa a funcao fechar modal
+    if (event.target === this.containerModal) {
+      this.toggleModal(); //ao clicar do lado de fora a funcao cliqueFora executa a funcao fechar modal
     }
   }
 
-  if (botaoAbrir && botaoFechar && containerModal) {
-    //se n existir retorna undefined q é false
-    botaoAbrir.addEventListener('click', toggleModal);
-    botaoFechar.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', cliqueForaModal);
+  //add os eventos aos elementos do modal
+  addModalEvents() {
+    this.botaoAbrir.addEventListener('click', this.eventToggleModal);
+    this.botaoFechar.addEventListener('click', this.eventToggleModal);
+    this.containerModal.addEventListener('click', this.cliqueForaModal);
+  }
+
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addModalEvents();
+    }
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
+import Modal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
@@ -22,7 +22,15 @@ const tabNav = new TabNav(
 );
 tabNav.init();
 
-initModal();
+const modal = new Modal(
+  "[data-modal='abrir']",
+  '[data-modal="fechar"]',
+  '[data-modal="container"]',
+);
+modal.init();
+
+//a grande vantagem de organizar o codigo em classes e de metodos oranizados e separados é usar todos os metodos apenas chamando exemplo chamando modal.toggleModal que inicia o modal logo de cara
+
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
