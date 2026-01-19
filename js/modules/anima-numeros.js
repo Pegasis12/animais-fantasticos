ec9ce92929724dc9faf3cb73a70452114586fa18
Pegasis12 <plsantana246@gmail.com>
2026-01-19 10:52:57 -0300

    refatorar anima numeros

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,37 +1,57 @@
-export default function initAnimaNumeros() {
-  function animaNumeros() {
-    //fiz uma funcao coloquei tudo dentro
-    const numeros = document.querySelectorAll('[data-numero]');
+export default class AnimaNumeros {
+  constructor(numeros, observerTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numeros);
+    this.observerTarget = document.querySelector(observerTarget);
+    this.observerClass = observerClass;
 
-    numeros.forEach((item) => {
-      const total = +item.innerText;
-      const incremento = Math.floor(total / 100);
-
-      let start = 0;
-      const timer = setInterval(() => {
-        start += incremento;
-        item.innerText = start;
-        if (start > total) {
-          item.innerText = total;
-          clearInterval(timer);
-        }
-      }, 25 * Math.random()); //um tempo diferente pra cada execução de cada item
-    });
+    this.handleMutation = this.handleMutation.bind(this);
   }
 
-  function handleMutation(mutation) {
+  // quando tenho uma funcao dentro de uma classe q n referencia nada da classe dentro dela podemos chamala de static
+  static incrementarNumero(numero) {
+    const total = +numero.innerText;
+    const incremento = Math.floor(total / 100);
+
+    let start = 0;
+    const timer = setInterval(() => {
+      start += incremento;
+      numero.innerText = start;
+      if (start > total) {
+        numero.innerText = total;
+        clearInterval(timer);
+      }
+    }, 25 * Math.random()); //um tempo diferente pra cada execução de cada item
+  }
+
+  // ativa incrementar numero para cada numero selecionadono dom
+  animaNumeros() {
+    this.numeros.forEach((numero) =>
+      this.constructor.incrementarNumero(numero),
+    ); //funcoes statics devem ser chamadas pelo construtor
+  }
+
+  // funcao que ocorre quando a mutacao ocorrrer
+  handleMutation(mutation) {
     //recebe um parametro especial omo se fosse o event, com ele podemos verificar se existea classe ativo
-    if (mutation[0].target.classList.contains('ativo')) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
       //se existir ativo ele disconecta o observador e anima os numeros
-      observer.disconnect(); //disconecta
-      animaNumeros(); //anima os numeros uma vez pq mesmo rodando agora ele ja vai esta desconectado
+      this.observer.disconnect(); //disconecta
+      this.animaNumeros(); //anima os numeros uma vez pq mesmo rodando agora ele ja vai esta desconectado
     }
   }
 
-  // MutationObserver é um objeto que observa um atributo ou algo se modificando
-  const observer = new MutationObserver(handleMutation); //recebe como argumento um callback
+  //add o MutationObserver para verificar quando  a classse ativo e adicionado ao target
+  addMutationObserver() {
+    // MutationObserver é um objeto que observa um atributo ou algo se modificando
+    this.observer = new MutationObserver(this.handleMutation); //recebe como argumento um callback
+    //um metodo de Mutation observe() pra identificar qual elemento ele deve observar, no caso ele observa nossa section numeros
+    this.observer.observe(this.observerTarget, { attributes: true }); //alem do argumento de oq observar ele tbm recebe mais argumentos, como um objeto de configuração, no nosso caso ele observa a modifição dos atributos da section numeros
+  }
 
-  const observerTarget = document.querySelector('.numeros'); //selecionei a section numeros
-  //um metodo de Mutation observe() pra identificar qual elemento ele deve observar, no caso ele observa nossa section numeros
-  observer.observe(observerTarget, { attributes: true }); //alem do argumento de oq observar ele tbm recebe mais argumentos, como um objeto de configuração, no nosso caso ele observa a modifição dos atributos da section numeros
+  init() {
+    if (this.numeros.lenght || this.observerTarget) {
+      this.addMutationObserver();
+    }
+    return this;
+  }
 }
--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from './anima-numeros.js'; //importando a animacao aq, pois ea precisa ser executada so depois do fetch, pq se n ele vai ser executado antes do fetch ser concluido e ja vai tentar animar algo que n existe
+import AnimaNumeros from './anima-numeros.js'; //importando a animacao aq, pois ea precisa ser executada so depois do fetch, pq se n ele vai ser executado antes do fetch ser concluido e ja vai tentar animar algo que n existe
 export default function initFetchAnimais() {
   async function fetchAnimais(url) {
     try {
@@ -12,8 +12,10 @@ export default function initFetchAnimais() {
         const divAnimal = createAnimal(animal);
         numeros.appendChild(divAnimal);
       });
-      initAnimaNumeros(); //executando a aniamcao depois do fetch, o fech conclui ai sim ele é executado
+      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+      animaNumeros.init();
     } catch (erro) {
+      console.log(erro);
     }
   }
 
