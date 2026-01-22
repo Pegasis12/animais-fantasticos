d657d8ad4ebadf271ca8a09cbe70df59394b15f7
Pegasis12 <plsantana246@gmail.com>
2026-01-22 15:32:15 -0300

    animacao ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,25 +3,45 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6; //pega a altura da tela e multiplica por 0,6 pra pega pelo menos 60% da tela
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      //precisamos do loop para saber a distancia de cada section do topo
-      const sectionTop = section.getBoundingClientRect().top; //usamos o metodo bound pois ele retorna diversas propriedades uteis como a distancia do topo cm top
-      const isSectionVisible = sectionTop - this.windowMetade < 0; //peguei a variavel da metade da tela e diminui menos a section top, e toda vez q estiver nesse calculo vai anima se for menor que 0
-      if (isSectionVisible) {
-        //se for true ocorre a animacao com a classe
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  //captura a distancia do top de cada sectione retonna um objeto com o elemento e a distancia offsetTop
+  getDistance() {
+    // quero o retorno do this.distance, por isso se usa map, pq map retorna o dado, porem preciso desestruturar o this.sections, pois ele n é uma array
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop; //offsetTop pega a distancia exata do top das secoes, mesmo cm scroll mostra a distancia da sessão até o topo
+      // retorno um objeto
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  //checa se a distancia do scroll em relacao a o ofsset da section e anima a mesma
+  checkDistance() {
+    // pageYOffset retorna o valor real do scroll no eixo y
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll); //anima o scroll do window q é o objeto maior
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance); //anima o scroll do window q é o objeto maior
+    }
+    return this;
+  }
+
+  //remove o evento, apenas deixei aq para caso queiramos fazer uma pausa caso algo ocorra temos aq
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
