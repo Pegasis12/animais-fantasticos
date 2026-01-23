fc69b45045fc825d1dbd7a3d94b22c79a6a9db2f
Pegasis12 <plsantana246@gmail.com>
2026-01-23 16:12:08 -0300

    refatorar-funcionnamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,26 +1,52 @@
-export default function initFuncionamento() {
+export default class Funcionamento {
   // o objetivo do script é comparar a data e hora do cliente pra saber se estamos fechados ou abertos, caso esteja fechado vai atribuir uma classe a uma bolinha em que ela vai ficar vermelha, cso esteja aberto a classe atribuida sera verde
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
+  }
 
-  const funcionamento = document.querySelector('[data-semana]');
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number); //macetinho se passar number dentro de map ele ja me reotrna number a array
+    this.horariosSemana = this.funcionamento.dataset.horario
+      .split(',')
+      .map(Number); //pega o horario dentro de data-horario
+  }
 
-  if (funcionamento) {
-    //Me retorna uma string do conteudo dentro do data-semana, para compara tenho q transformar em array, pra isso do split em (,) porem ainda n acabou pois ele me retorna a array com string, e quero number, pra isso podemos interagir com map, lembrando q ele intera na array e retorna uma array nova modificada, diferente do foreach
-    const diasSemana = funcionamento.dataset.semana.split(',').map(Number); //macetinho se passar number dentro de map ele ja me reotrna number a array
-    const horariosSemana = funcionamento.dataset.horario.split(',').map(Number); //pega o horario dentro de data-horario
+  //Me retorna uma string do conteudo dentro do data-semana, para compara tenho q transformar em array, pra isso do split em (,) porem ainda n acabou pois ele me retorna a array com string, e quero number, pra isso podemos interagir com map, lembrando q ele intera na array e retorna uma array nova modificada, diferente do foreach
 
-    const dataAgora = new Date();
-    const diaAgora = dataAgora.getDay();
-    const horarioAgora = dataAgora.getHours(); //pega exatamente o horario de agora
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horarioAgora = this.dataAgora.getUTCHours() - 3; //pega exatamente o horario de agora, utc pra se estiver em outro pais ele pegue o time corretmanete -3 do brasil
+  }
 
-    const semanaAberto = diasSemana.indexOf(diaAgora) !== -1; //Se for diferente de menos 1 é verdadeiro. A expressão me retorna onde esta o index na array exemplo, hoje é dia 4 os dias que tem dentro de dias semana são [1,2,3,4,5] ele vai verificar onde meu dia de hoje esta na array, no caso na posição 3 do array que é 4, caso n encontre me retorna -1
+  estaAberto() {
+    const semanaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1; //Se for diferente de menos 1 é verdadeiro. A expressão me retorna onde esta o index na array exemplo, hoje é dia 4 os dias que tem dentro de dias semana são [1,2,3,4,5] ele vai verificar onde meu dia de hoje esta na array, no caso na posição 3 do array que é 4, caso n encontre me retorna -1
     const horarioAberto =
-      horarioAgora >= horariosSemana[0] && horarioAgora < horariosSemana[1]; //verifica se o horario de agora é maior que 8 e menor q 18
+      this.horarioAgora >= this.horariosSemana[0] &&
+      this.horarioAgora < this.horariosSemana[1]; //verifica se o horario de agora é maior que 8 e menor q 18
 
-    if (semanaAberto && horarioAberto) {
-      funcionamento.classList.add('aberto');
+    return semanaAberto && horarioAberto; //retorna true ou false, com base nas operacoes acima
+  }
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      //se o retorno do metodo for true ele roda a classe aberto
+      this.funcionamento.classList.add(this.activeClass);
     }
   }
+
+  init() {
+    if (this.funcionamento) {
+      //ativando os metodos de dadosFuncionamento e dadosagora pois eles precisam rodas pra funcionar e depois fazer o ativaaberto
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
+    return this;
+  }
 }
+
 // // new Date()
 // // O construtor Date cria um objeto contendo valores como mês, dia, ano, horário e mais. A data é baseada no relógio interno do computador.
 // const agora = new Date();
